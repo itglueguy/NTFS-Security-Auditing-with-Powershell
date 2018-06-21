a034f7e77fd7d0f364584f499fefab4fef729a8a
itglueguy <38425660+itglueguy@users.noreply.github.com>
2018-06-21 15:38:39 -0700

    more lerned
    ddd

--- NTFS_auditing.ps1
+++ NTFS_auditing.ps1
@@ -23,32 +23,32 @@ Function Get-AclfromWindows ($computer,$depth) {
     # get all the valid share letters from the computer
     $shares = (get-WmiObject -class Win32_Share -computer $computer | where {$_.Name -like "*$*" -and $_.Name -ne "C$" -and $_.Name -ne "ADMIN$" -and $_.Name -ne "IPC$"} | select Name).Name
 
-    # Curse as it helps with getting the Bad ACL's out with Powershell
-    $dang_results = foreach($share in $shares) {
+    # get all the ACLS from the shares of a computer
+    $ACL_Results = foreach($share in $shares) {
 
         # filter it to the base shares only and no user folders
         $full_share_path = -join("\\",$computer,"\",$share)
 
         $folders = Get-childitem $full_share_path -recurse -depth $depth -Directory | % { $path1 = $_.fullname; Get-Acl $_.Fullname | % { $_.access | Add-Member -MemberType NoteProperty 'Path' -Value $path1 -passthru }}
 
-                        # the $folders object will then report all the fields to the $all_results object
-                        $folders
+        # the $folders object will then report all the fields to the $all_results object
+        $folders
 
                     }
-    # when done it will output the $dang_results object
-    $dang_results
+    # when done it will output the $ACL_Results object
+    $ACL_Results
 
 }
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 
-Function Get-OrderedACLbyKeyword ($computer,$keywords) {
+Function Get-ACLbyKeyword ($computer,$volumefilter,$keywords) {
 
     # Query all the open files from a certain computer
     $openfiles = openfiles /Query /S $computer /FO CSV /V | ConvertFrom-Csv
 
     # further Filter those files
-    $filtered = $openfiles | Select "Accessed By","Open Mode","Open File (Path\executable)"
+    $filtered = $openfiles | where {$_."Open File (Path\executable)" -like "*$volumefilter*"} | Select "Accessed By","Open Mode","Open File (Path\executable)"
 
 
     foreach($keyword in $keywords) {
@@ -121,6 +121,8 @@ Function Get-OrderedACLbyKeyword ($computer,$keywords) {
         Enable-NTFSAccessInheritance -path $path -RemoveExplicitAccessRules
 
     }
+
+}
 
 
 
